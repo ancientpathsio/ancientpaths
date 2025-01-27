1dac8f95d6223d8a66a94f0978f5b19095186a0d
AncientPaths.io <ancientpaths.io@gmail.com>
2025-01-26 20:38:38 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://discord.gg/xk6qxyZSkx',
-            label: 'Community (Discord)',
+            label: ':discord: Community (Discord)',
             position: 'right',
           },
         ],
