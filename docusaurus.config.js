de9db0dbc2b95a4c9d5a0ef87ec5bc7869126641
AncientPaths.io <ancientpaths.io@gmail.com>
2025-01-26 20:42:31 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,8 +72,13 @@ const config = {
           { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://discord.gg/xk6qxyZSkx',
-            label: ':discord: Community (Discord)', // Added emoji here
+            // Use an icon instead of the label
             position: 'right',
+            className: 'navbar-icon', // Add custom class for the icon
+            'aria-label': 'Discord Community', // Accessibility feature
+            // You can include a Discord icon image like this
+            // Replace the src URL with your actual icon path or URL
+            html: `<img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Discord_Icon.svg" alt="Discord" style="height: 30px; width: 30px;" />`,
           },
         ],
       },
@@ -97,8 +102,8 @@ const config = {
                 href: 'https://stackoverflow.com/questions/tagged/docusaurus',
               },
               {
-                label: 'Discord',
                 href: 'https://discord.gg/xk6qxyZSkx',
+                html: `<img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Discord_Icon.svg" alt="Discord" style="height: 20px; width: 20px;" />`, // Discord icon
               },
               {
                 label: 'Twitter',
