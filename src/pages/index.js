f11f1f0369112272505a20a327db3e20e4401eaf
AncientPaths.io <ancientpaths.io@gmail.com>
2025-01-26 19:32:15 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title={``}
       description="">
       <HomepageHeader />
       <main>
