9490030542c78c0a814e345f3db75a098f327a3c
AncientPaths.io <ancientpaths.io@gmail.com>
2025-02-05 11:46:49 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
         { to: '/blog', label: 'Dev_', position: 'left' },
         {
           label: 'Donate',
-          href: '/give',
+          href: 'https://ancientpaths.io/give',
           position: 'right',
           className: 'navbar__item--donate',
         },
