77f77a33239e5ce155b1b8c886989bdd4c527674
AncientPaths.io <ancientpaths.io@gmail.com>
2025-02-10 09:41:23 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
           type: 'doc',
           docId: 'guidebook',  // Pointing to docs/guidebook.md
           position: 'left',
-          label: 'Guidebook',  // Updated label to "Guidebook"
+          label: 'The Guidebook',  // Updated label to "Guidebook"
         },
         { to: '/blog', label: 'Dev_', position: 'left' },
         {
