d24f43b1ff6f7ed614cc5c1becb82a83d4c26369
AncientPaths.io <ancientpaths.io@gmail.com>
2025-02-18 11:03:07 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,10 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import styles from './index.module.css';
+import RefreshOnFirstLoad from '@site/src/components/HomepageFeatures/RefreshOnFirstLoad';
+
+<RefreshOnFirstLoad />
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
