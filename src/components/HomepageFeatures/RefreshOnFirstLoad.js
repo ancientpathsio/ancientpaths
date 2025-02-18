e7897c5497a5f8fe225da74ad388f349ca2580ec
AncientPaths.io <ancientpaths.io@gmail.com>
2025-02-18 10:46:20 -0500

    Update RefreshOnFirstLoad.js

--- src/components/HomepageFeatures/RefreshOnFirstLoad.js
+++ src/components/HomepageFeatures/RefreshOnFirstLoad.js
@@ -1,16 +1,12 @@
 import React, { useEffect } from 'react';
 
-const RefreshOnFirstLoad = () => {
+const RefreshOnPageLoad = () => {
   useEffect(() => {
-    if (!localStorage.getItem('hasVisited')) {
-      // Mark as visited
-      localStorage.setItem('hasVisited', 'true');
-      // Trigger a page reload
-      window.location.reload();
-    }
-  }, []);
+    // Trigger a page reload
+    window.location.reload();
+  }, []); // Empty dependency array ensures this effect runs once per page load
 
   return null; // This component does not render anything
 };
 
-export default RefreshOnFirstLoad;
+export default RefreshOnPageLoad;
