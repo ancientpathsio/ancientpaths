38ece8f013cf55c478104f15d06382dcda358098
AncientPaths.io <ancientpaths.io@gmail.com>
2025-02-18 11:14:13 -0500

    Update BlueLetterBibleScript.js

--- src/components/HomepageFeatures/BlueLetterBibleScript.js
+++ src/components/HomepageFeatures/BlueLetterBibleScript.js
@@ -6,7 +6,7 @@ const BlueLetterBibleScript = () => {
     script.src = 'https://www.blueletterbible.org/assets-v3/scripts/blbToolTip/BLB_ScriptTagger-min.js';
     script.type = 'text/javascript';
     script.onload = () => {
-      // Your script initialization code here
+      // Additional settings for the plugin after it loads
       window.BLB.Tagger.Translation = 'ESV';
       window.BLB.Tagger.HyperLinks = 'all';
       window.BLB.Tagger.HideTanslationAbbrev = false;
@@ -17,11 +17,11 @@ const BlueLetterBibleScript = () => {
     document.head.appendChild(script);
 
     return () => {
-      document.head.removeChild(script);
+      document.head.removeChild(script); // Cleanup the script when the component is unmounted
     };
   }, []);
 
-  return null; // This component doesn't need to render anything
+  return null; // This component doesn't render anything visually
 };
 
 export default BlueLetterBibleScript;
