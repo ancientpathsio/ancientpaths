3314328b30a5ffc49b870c6b11d353114a579995
AncientPaths.io <ancientpaths.io@gmail.com>
2025-02-18 11:09:53 -0500

    Delete static/js directory

--- static/js/reftagger-config.js
+++
@@ -1,8 +0,0 @@
-window.RefTagger = {
-  settings: {
-    bibleVersion: "ESV",  // Change to preferred Bible version
-    tagChapters: true,
-    tooltipStyle: "dark",
-    socialSharing: ["twitter", "facebook"],  // Enable social sharing
-  },
-};
