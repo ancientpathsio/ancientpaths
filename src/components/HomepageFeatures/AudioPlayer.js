88f86ad9e4ca4ad934b260749d6bea57f4c7ada1
AncientPaths.io <ancientpaths.io@gmail.com>
2025-02-25 16:44:01 -0500

    Update AudioPlayer.js

--- src/components/HomepageFeatures/AudioPlayer.js
+++ src/components/HomepageFeatures/AudioPlayer.js
@@ -51,7 +51,7 @@ const AudioMsrPlayer = () => {
 
       <div
         style={{
-          maxHeight: "150px",
+          maxHeight: "350px",
           overflowY: "auto",
           border: "1px solid #ccc",
           padding: "10px",
